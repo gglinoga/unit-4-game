28df73e27ffe3cc3b974ff7f2eadb64122ad8a28
Greg Glinoga <gglinoga@gmail.com>
2019-03-18 12:27:26 -0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # unit-4-game
-Assignment 4
+## Assignment 4
+
+This is the Unit 4 game assignment, Crystal Collector.  The game utilizes jQuery to dynamically update the HTML page.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 		wins: 0,
 		losses: 0,
 		startup: function() {
-			this.score = (Math.floor(Math.random() * 12) + 1);
+			this.score = 0;
 			$(".score").html(this.score);
 			this.goal = (Math.floor(Math.random() * 101) + 19);
 			$(".goal").html(this.goal);
@@ -19,10 +19,14 @@ $(document).ready(function() {
 		},
 		roundover: function() {
 			if (this.score === this.goal) {
+				let audio = new Audio('assets/sounds/win.mp3');
+				audio.play();
 				alert("Congratulations, you have won!");
 				this.wins++;
 				this.startup();
 			} else if (this.score > this.goal) {
+				let audio = new Audio('assets/sounds/lose.mp3');
+				audio.play();
 				alert("Sorry, you have lost!");
 				this.losses++;
 				this.startup();
@@ -33,21 +37,29 @@ $(document).ready(function() {
 	$(".button0").on("click", function() {
 		cc.score += cc.buttons[0];
 		$(".score").html(cc.score);
+		let audio = new Audio('assets/sounds/click.mp3');
+		audio.play();
 		cc.roundover();
 	});
 	$(".button1").on("click", function() {
 		cc.score += cc.buttons[1];
 		$(".score").html(cc.score);
+		let audio = new Audio('assets/sounds/click.mp3');
+		audio.play();
 		cc.roundover();
 	});
 	$(".button2").on("click", function() {
 		cc.score += cc.buttons[2];
 		$(".score").html(cc.score);
+		let audio = new Audio('assets/sounds/click.mp3');
+		audio.play();
 		cc.roundover();
 	});
 	$(".button3").on("click", function() {
 		cc.score += cc.buttons[3];
 		$(".score").html(cc.score);
+		let audio = new Audio('assets/sounds/click.mp3');
+		audio.play();
 		cc.roundover();
 	});
 }); //ready wrapper end
