cf06767f9f7156f4a85207e00291a5ce35c9312d
Greg Glinoga <gglinoga@gmail.com>
2019-03-18 20:19:31 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,67 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	var cc = {
 		score: 0,
 		buttons: [0, 0, 0, 0],
 		goal: 0,
 		wins: 0,
 		losses: 0,
-		startup: function() {
+		startup: function () {
 			this.score = 0;
 			$(".score").html(this.score);
 			this.goal = (Math.floor(Math.random() * 101) + 19);
 			$(".goal").html(this.goal);
 			$(".wins").html(this.wins);
 			$(".losses").html(this.losses);
-			console.log(cc.buttons); ///cheat codes
 			for (var i = 0; i < this.buttons.length; i++) {
 				this.buttons[i] = (Math.floor(Math.random() * 12) + 1);
 			}
+			console.log(cc.buttons); ///cheat codes
 		},
-		roundover: function() {
+		roundover: function () {
+			$(".score").html(this.score);
 			if (this.score === this.goal) {
 				let audio = new Audio('assets/sounds/win.mp3');
 				audio.play();
-				alert("Congratulations, you have won!");
+				winmsg = function () {
+					alert("Congratulations, you have won!");
+				}
+				setTimeout(winmsg, 1000);
 				this.wins++;
 				this.startup();
 			} else if (this.score > this.goal) {
 				let audio = new Audio('assets/sounds/lose.mp3');
 				audio.play();
+				losemsg= function (){
 				alert("Sorry, you have lost!");
+				}
+				setTimeout(losemsg, 1000);
 				this.losses++;
 				this.startup();
 			}
+		},
+		calc: function (x) {
+			cc.score += cc.buttons[x];
+			$(".score").html(cc.score);
+			let audio = new Audio('assets/sounds/click.mp3');
+			audio.play();
+			cc.roundover();
 		}
 	} //var cc end
 	cc.startup();
-	$(".button0").on("click", function() {
-		cc.score += cc.buttons[0];
-		$(".score").html(cc.score);
-		let audio = new Audio('assets/sounds/click.mp3');
-		audio.play();
-		cc.roundover();
+	$(".button0").on("click", function () {
+		buttonVal = 0;
+		cc.calc(buttonVal);
 	});
-	$(".button1").on("click", function() {
-		cc.score += cc.buttons[1];
-		$(".score").html(cc.score);
-		let audio = new Audio('assets/sounds/click.mp3');
-		audio.play();
-		cc.roundover();
+	$(".button1").on("click", function () {
+		buttonVal = 1;
+		cc.calc(buttonVal);
 	});
-	$(".button2").on("click", function() {
-		cc.score += cc.buttons[2];
-		$(".score").html(cc.score);
-		let audio = new Audio('assets/sounds/click.mp3');
-		audio.play();
-		cc.roundover();
+	$(".button2").on("click", function () {
+		buttonVal = 2;
+		cc.calc(buttonVal);
 	});
-	$(".button3").on("click", function() {
-		cc.score += cc.buttons[3];
-		$(".score").html(cc.score);
-		let audio = new Audio('assets/sounds/click.mp3');
-		audio.play();
-		cc.roundover();
+	$(".button3").on("click", function () {
+		buttonVal = 3;
+		cc.calc(buttonVal);
 	});
 }); //ready wrapper end
